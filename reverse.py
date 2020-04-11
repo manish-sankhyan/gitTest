1b2f760ead4d23667a24ecb7ec77508e1e1ec458
ma.kumar <sankhyanmanish@gmail.com>
2020-04-11 15:10:42 +0530

    branch1 script changes/modifies

--- reverse.py
+++ reverse.py
@@ -1,8 +1,8 @@
 def reverse(s): 
-  str = "" 
-  for i in s: 
-    str = i + str
-  return str
+   if len(s) == 0: 
+        return s 
+    else: 
+        return reverse(s[1:]) + s[0] 
   
 s = "Geeksforgeeks"
   
